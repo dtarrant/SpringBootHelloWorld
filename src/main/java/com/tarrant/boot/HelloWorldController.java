3b85dc3476ee8a3f92e97063c4ecd77b2cb6778a
d.tarrant <d.tarrant@animalhealthmonitoring.com>
2014-08-06 12:51:00 +0100

    Initial commit

---
+++ src/main/java/com/tarrant/boot/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.tarrant.boot;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloWorldController.class, args);
+    }
+}
